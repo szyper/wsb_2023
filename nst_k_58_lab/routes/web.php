1ad003e7f8ac5224378b12a3a3db6620ed1931c4
szyper <zskgit@gmail.com>
2023-10-01 18:19:37 +0200

    laravel

--- nst_k_58_lab/routes/web.php
+++ nst_k_58_lab/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,35 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('address', function(){
+    return 'test';
+});
+
+Route::get('address2/{city}/{street}/{zipCode}', function(string $city, string $street, int $zipCode){
+    $zipCode = substr($zipCode,0,2).'-'.substr($zipCode, 2, 3);
+    echo <<< ADDRESS
+      Kod pocztowy: $zipCode<br>
+      Miasto: $city, ulica: $street
+ADDRESS;
+});
+
+Route::get('address3/{city?}/{street?}/{zipCode?}', function(string $city = '-', string $street = '', int $zipCode = null){
+  $zipCode = (is_null($zipCode)) ? 'brak kodu pocztowego' : substr($zipCode,0,2).'-'.substr($zipCode, 2, 3);
+  echo <<< ADDRESS
+      Kod pocztowy: $zipCode<br>
+      Miasto: $city, ulica: $street
+ADDRESS;
+})->name('adres_miasto');
+
+Route::redirect('adres/{city?}/{street?}/{zipCode?}', '/address3/{city?}/{street?}/{zipCode?}');
+
+Route::redirect('stronaglowna', '/');
+Route::redirect('test', 'stronaglowna');
+
+//Route::get('pages', [\App\Http\Controllers\PageController::class, 'show']);
+Route::get('pages', [PageController::class, 'show']);
+Route::get('drives/{drive}', [PageController::class, 'show']);
+
+Route::view('userform', 'userform');
+Route::post('UserController', [\App\Http\Controllers\UserController::class, 'showUser']);
