ff93028d37cce7e829323eee87e56d0564f58f1b
szyper <31780468+szyper@users.noreply.github.com>
2023-12-13 13:33:26 +0100

    wsb

---
+++ st/dIinz_2_K15_Inf/7_2_delegaty/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Program
+{
+    //7.1 delegaty
+    public delegate bool Logic(bool x, bool y);
+    internal class Program
+    {
+        public static bool And(bool x, bool y)
+        {
+            return x && y;
+        }
+        public static bool Or(bool x, bool y)
+        {
+            return x || y;
+        }
+        public static bool Xor(bool x, bool y)
+        {
+            return x ^ y;
+        }
+        public static bool Not(bool x, bool y)
+        {
+            return !x;
+        }
+        static void Main(string[] args)
+        {
+            bool x = getBoolFromUser();
+            bool y = getBoolFromUser();
+
+            Logic and = new Logic(And);
+            DisplayResult(and, x, y);
+
+            DisplayResult(new Logic(Or), x, y);
+            DisplayResult(new Logic(Xor), x, y);
+            DisplayResult(new Logic(Not), x, y);
+        }
+        public static void DisplayResult(Logic logic, bool x, bool y)
+        {
+            try
+            {
+                bool result = logic(x, y);
+                Console.WriteLine("\nWynik operacji {0} na liczbach {1} i {2} wynosi {3}", logic.Method.Name, x, y, result);
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Wystąpił błąd: {0}", e.Message);
+            }
+        }
+
+        public static bool getBoolFromUser()
+        {
+            while(true)
+            {
+                Console.Write("Wprowadź wartość boolowską (true/false):");
+                string input = Console.ReadLine();
+
+                bool value;
+                if (bool.TryParse(input, out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.Write("Błędne dane. ");
+                }
+            }
+        }
+    }
+}
---
+++ st/dIinz_2_K15_Inf/8_1_1_zdarzenia/Program.cs
@@ -0,0 +1,65 @@
+﻿using static _8_1_1_zdarzenia.Publisher;
+
+namespace _8_1_1_zdarzenia
+{
+    public delegate void MessageHandler(string message, int priority);
+
+    public class Publisher
+    {
+        public event MessageHandler MessageEvent;
+
+        public void SendMessage(string message, int priority)
+        {
+            if (MessageEvent != null)
+            {
+                MessageEvent(message, priority);
+            }
+        }
+
+        public class Subscriber
+        {
+            public Subscriber (int threshold, string name)
+            {
+                this.Threshold = threshold;
+                this.Name = name;
+            }
+            public int Threshold { get; set; }
+            public string Name { get; set; }
+
+            public void OnMessageReceived(string message, int priority)
+            {
+                if (priority <= Threshold)
+                {
+                    Console.WriteLine("{0} otrzymał wiadomość: {1} o priorytecie: {2}", Name, message, priority);
+                }
+                else
+                {
+                    Console.WriteLine("{0} nie otrzymał wiadomości: {1} o priorytecie: {2}", Name, message, priority);
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher pub = new Publisher();
+            Subscriber sub1 = new Subscriber(3, "Subskrybent 1");
+            Subscriber sub2 = new Subscriber(4, "Subskrybent 2");
+
+            pub.MessageEvent += sub1.OnMessageReceived;
+
+            pub.SendMessage("Pierwsza wiadomość", 1);
+            pub.MessageEvent += sub2.OnMessageReceived;
+
+            pub.SendMessage("Druga wiadomość", 2);
+
+
+
+            pub.SendMessage("Trzecia wiadomość", 3);
+            pub.SendMessage("Czwarta wiadomość", 4);
+
+
+        }
+    }
+}
