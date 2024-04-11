399aba084bf696f886f7265bd903caccc6d3bd1f
szyper <31780468+szyper@users.noreply.github.com>
2024-04-11 07:23:08 +0200

    wsb

---
+++ st/dIinz_2_K15_Inf/13_pakowanie/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_pakowanie
+{
+  internal class Program
+  {
+    class BoxingUnboxing
+    {
+      public object Boxing(int value)
+      {
+        return value;
+      }
+
+      public int Unboxing(object obj)
+      {
+        return (int)obj;
+      }
+
+      public byte[] Pack(int[] values)
+      {
+        byte[] packedValues = new byte[values.Length * sizeof(int)];
+        Buffer.BlockCopy(values, 0, packedValues, 0, values.Length);
+        return packedValues;
+      }
+
+      public int[] Unpack(byte[] packedValues)
+      {
+        int[] values = new int[packedValues.Length / sizeof(int)];
+        Buffer.BlockCopy(packedValues, 0, values, 0, packedValues.Length);
+        return values;
+      }
+    }
+    static void Main(string[] args)
+    {
+      BoxingUnboxing boxingUnboxing = new BoxingUnboxing();
+
+      object boxedValue = boxingUnboxing.Boxing(42);
+      int unboxedValue = boxingUnboxing.Unboxing(boxedValue);
+      Console.WriteLine($"unboxedValue: {unboxedValue}");
+
+      int[] values = new int[] { 1, 2, 3, 4, 5 };
+      byte[] packedValues = boxingUnboxing.Pack(values);
+      Console.WriteLine($"packedValues: {BitConverter.ToString(packedValues)}");
+
+      int[] unpackedValues = boxingUnboxing.Unpack(packedValues);
+      Console.WriteLine($"unpackedValues: {string.Join(", ", unpackedValues)}");
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ st/dIinz_2_K15_Inf/14_indeksatory/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_indeksatory
+{
+  internal class Program
+  {
+    class Dictionary
+    {
+      private string[,] data;
+      public Dictionary()
+      {
+        data = new string[100, 2];
+      }
+
+      public int GetLength()
+      {
+        return data.Length;
+      }
+
+      public string this[string key]
+      {
+        get
+        {
+          for (int i = 0; i < data.GetLength(0); i++)
+          {
+            if (data[i, 0] == key)
+            {
+              return data[i, 1];
+            }
+          }
+          throw new KeyNotFoundException();
+        }
+
+        set
+        {
+          for (int i = 0; i < data.GetLength(0); i++)
+          {
+            if (data[i, 0] == key)
+            {
+              data[i, 1] = value;
+              return;
+            }
+            if (data[i, 0] == null)
+            {
+              data[i, 0] = key;
+              data[i, 1] = value;
+              return;
+            }
+          }
+          throw new IndexOutOfRangeException();
+        }
+      }
+    }
+    static void Main(string[] args)
+    {
+      Dictionary dict = new Dictionary();
+      Console.WriteLine(dict.GetLength()); //200
+
+      dict["apple"] = "jabłko";
+      dict["banana"] = "banan";
+      Console.WriteLine(dict["banana"]); //banan
+
+      Console.WriteLine(dict["test"]);
+
+      Console.ReadKey();
+    }
+  }
+}
